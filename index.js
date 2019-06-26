1003d8fd5e8842befc8a5973445d17f6ddf05d06
Michael Kurowski <mkurowski@divante.pl>
2019-06-26 13:43:42 +0200

    draft

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const http = require('http')
+const express = require('express')
+
+const app = express()
+app.use(express.static(process.env.SERVE_DIRECTORY || 'dist'))
+app.get('/', function(req, res) {
+  return res.end('<p>This server serves up static files.</p>')
+})
+
+const server = http.createServer(app)
+
+server.listen(process.env.SERVER_PORT || 8443)
