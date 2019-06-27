5eb7fcf06420b87aec85daba6e73faf88fbcb6fb
michaelKurowski <Byamarro@gmail.com>
2019-06-27 12:31:09 +0200

    satellite

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const fs = require('fs')
 const http = require('http')
 const express = require('express')
+const request = require('request');
 
 const app = express()
-app.use(express.static(process.env.SERVE_DIRECTORY || 'dist'))
+// app.use(express.static(process.env.SERVE_DIRECTORY || 'dist'))
+let status = {}
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-  return res.end('<p>This server serves up static files.</p>')
-})
+  var drinks = [
+      { name: 'Bloody Mary', drunkness: 3 },
+      { name: 'Martini', drunkness: 5 },
+      { name: 'Scotch', drunkness: 10 }
+  ];
+  res.render(__dirname +'/dist/index.ejs', {...status.iss_position, timestamp: new Date().getTime()});
+});
 
 const server = http.createServer(app)
 
 server.listen(process.env.PORT || 8443)
+
+
+
+
+
+setInterval(() => {
+  request('http://api.open-notify.org/iss-now.json', { json: true }, (err, res, body) => {
+    status = body
+  })
+}, 1000)
