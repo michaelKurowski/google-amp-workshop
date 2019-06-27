2d97818f3bd79eaf3d10a77c8ddd7c1961ba04e6
michaelKurowski <Byamarro@gmail.com>
2019-06-27 12:58:06 +0200

    fixed assets getting

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const request = require('request');
 const path = require('path')
 
 const app = express()
-// app.use(express.static(process.env.SERVE_DIRECTORY || 'dist'))
+app.use(express.static(process.env.SERVE_DIRECTORY || 'dist'))
 let status = {}
 app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
   res.render(__dirname + '/dist/index.ejs', {...status.iss_position, timestamp: new Date().getTime()});
 });
